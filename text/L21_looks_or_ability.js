e1c30daa9d8335ee5a987297cec28ebd2daba2ef
Julian Laffey <jules1975@pm.me>
2020-08-16 10:12:33 +1000

    Create another audio asset

---
+++ text/L21_looks_or_ability.js
@@ -0,0 +1,13 @@
+
+const data = {
+    'title': "Level 21 - Looks or ability?",
+    'attribution': 'Audio content &copy; <a href="https://mandarinblueprint.com">Mandarin Blueprint</a>',
+    'src': "audio/L21_looks_or_ability.mp3",
+    'text': {
+        '00': 'A:你觉得长相重要还是能力重要？',
+        '03': 'B:我觉得长相重要一点。你看，现在有两个人去找工作。',
+        '05': '一个长得看，但另一个长得不好看。',
+        '07': '你是老板的话，你会要谁？',
+        '999': ""
+    }
+};
