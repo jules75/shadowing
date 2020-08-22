57eb959475b75cb0c29bb4e28e303f9aad060be4
Julian Laffey <j.laffey@federation.edu.au>
2020-08-22 10:22:55 +1000

    Transferring money (female)

---
+++ text/L21_transferring_money_female.js
@@ -0,0 +1,18 @@
+
+const data = {
+    'title': "Level 21 - Transferring Money by smartphone or bank",
+    'attribution': 'Audio content &copy; <a href="https://mandarinblueprint.com">Mandarin Blueprint</a>',
+    'src': "audio/L21_transferring_money_female.mp3",
+    'text': {
+        '00': "现在在中国，越来越多人用手机给钱，",
+        '06': "所以有些人担心银行可能会关门，",
+        '11': "因为用手机比去银行方便得多。",
+        '16': "比如现在，你不但可以在手机银行上给你们寄钱，",
+        '23': "你还可以在手机上用WeChat或者Alipay给你们转钱，",
+        '30': "这样不是又快又安全吗？",
+        '33': "但是，有另一些人，",
+        '36': "你们认为去银行寄钱取钱比用手机给钱安全一些，",
+        '44': "所以他们还是会去银行。",
+        '999': ""
+    }
+};
